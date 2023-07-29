3e26f1f726076f89af2ed83c410ad6b2ed48b98f
Jordan Lopez <jordanlopezemail@gmail.com>
2023-07-28 23:32:26 -0700

    all endpoints work now

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ app.get('/notes',(req,res)=>{
     res.sendFile(path.join(__dirname, "public" ,'notes.html')); 
 })
 
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'public',"index.html"));
+
+});
+
+
+
 
 
 
