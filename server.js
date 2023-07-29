28e286867f958ee10b81469542e93aeb99c94329
Jordan Lopez <jordanlopezemail@gmail.com>
2023-07-29 00:06:30 -0700

    i have the api/notes route working

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
+
 const app = express();
 
 const PORT = 3000;
 
+app.use(express.json());
 app.use(express.static('public'));
 
 
@@ -13,10 +15,25 @@ app.get('/notes',(req,res)=>{
     res.sendFile(path.join(__dirname, "public" ,'notes.html')); 
 })
 
+
+app.get('/api/notes',(req, res)=>{
+    fs.readFile('./db/db.json','utf-8',(error, data)=>{
+        error ? console.error("Error reading this file:", err) : console.log('success');
+        let output = JSON.parse(data);
+        res.json(output);
+    });
+});
+
+
 app.get('*',(req,res)=>{
     res.sendFile(path.join(__dirname, 'public',"index.html"));
 
 });
+
+
+// app.post('/api/notes',(req,res)=>{
+
+// });
 
 
 
