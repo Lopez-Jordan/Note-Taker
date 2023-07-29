bdc20b9a25e3188e2c95c93a08653c89508a0b61
Jordan Lopez <jordanlopezemail@gmail.com>
2023-07-28 23:01:12 -0700

    trying to debug my api endpoints

--- Develop/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const fs = require('fs');
-
-const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());                // when the fetch passes in JSON content
-app.use(express.static('public'));      // allows access to paths within the public folder
-
